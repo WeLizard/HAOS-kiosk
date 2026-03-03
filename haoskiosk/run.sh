3d05aba019ac73a4f801c702d81638f0507d32ae
WeLizard <lizardjazz1@yandex.ru>
2026-03-03 22:50:00 +0300

    Enable Chromium touch events for kiosk

--- haoskiosk/run.sh
+++ haoskiosk/run.sh
@@ -3,7 +3,7 @@
 ################################################################################
 # Add-on: HAOS Kiosk Display (haoskiosk)
 # File: run.sh
-# Version: 1.3.0
+# Version: 1.3.1-welizard.3
 # Copyright Jeff Kosowsky
 # Date: February 2026
 #
@@ -202,6 +202,8 @@ resolve_browser_binary() {
                 --window-position=0,0
                 --start-fullscreen
                 --kiosk
+                --ozone-platform=x11
+                --touch-events=enabled
                 --enable-gpu-rasterization
                 --ignore-gpu-blocklist
                 --use-gl=egl
